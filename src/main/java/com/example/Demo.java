3487119f1b524ce17c4b60fe3b431eb85193b34a
obaibula <baibula.oo@gmail.com>
2023-08-05 19:44:54 +0300

    commit

--- src/main/java/com/example/Demo.java
+++ src/main/java/com/example/Demo.java
@@ -1,2 +1,35 @@
-package com.example;public class Demo {
+package com.example;
+
+import com.example.entity.Person;
+import jakarta.persistence.Persistence;
+import org.hibernate.Session;
+
+public class Demo {
+    public static void main(String[] args) {
+        var emf = Persistence.createEntityManagerFactory("default");
+        var em = emf.createEntityManager();
+        /*em.unwrap(Session.class)
+                .setDefaultReadOnly(true);*/
+
+        try {
+            em.getTransaction().begin();
+
+            /*var person = new Person();
+            person.setEmail("poshta@mail.com");
+            person.setFirstName("Petro");
+            person.setLastName("Poroshenko");
+            em.persist(person);*/
+            var person = em.find(Person.class, 5L);
+            em.detach(person);
+            person.setFirstName("Viktor");
+            em.merge(person);
+
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
 }
--- src/main/java/com/example/entity/Person.java
+++ src/main/java/com/example/entity/Person.java
@@ -1,2 +1,54 @@
-package com.example.entity;public class Person {
+package com.example.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "persons")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ?
+                ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass()
+                :
+                o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ?
+                ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass()
+                :
+                this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Person person = (Person) o;
+        return getId() != null && Objects.equals(getId(), person.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ?
+                ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode()
+                :
+                getClass().hashCode();
+    }
 }
