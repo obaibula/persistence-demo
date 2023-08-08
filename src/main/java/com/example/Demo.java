3872c40c5ad1ef74bc14b31c20d63f362dddd516
obaibula <baibula.oo@gmail.com>
2023-08-08 08:12:36 +0300

    commit4

--- src/main/java/com/example/Demo.java
+++ src/main/java/com/example/Demo.java
@@ -1,18 +1,12 @@
 package com.example;
 
-import com.codahale.metrics.ConsoleReporter;
 import com.codahale.metrics.MetricRegistry;
 import com.example.entity.Change;
-import com.example.entity.Person;
 import com.example.entity.StatusType;
 import com.example.entity.Task;
 import jakarta.persistence.Persistence;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.stat.Statistics;
 
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 public class Demo {
     static final MetricRegistry metrics = new MetricRegistry();
@@ -25,16 +19,13 @@ public class Demo {
         try {
             em.getTransaction().begin();
 
-           /* var task = new Task();
+            var task = new Task();
             task.setStatus(StatusType.NEW);
             var change = new Change();
             change.setChangedBy("Oleh");
             change.setChangedOn(new Date(System.currentTimeMillis()));
             task.setChange(change);
-            em.persist(task);*/
-            var task = em.find(Task.class, 1L);
-            System.out.println(task);
-            task.getChange().setChangedBy("Petro");
+            em.persist(task);
 
             em.getTransaction().commit();
         } catch (Exception e) {
